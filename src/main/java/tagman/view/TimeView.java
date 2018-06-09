2f1c540aaf6171265a0f2846016d83ac1dc415d8
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-06-09 21:05:31 +0200

    :bug: Make sure TimeView implements Observer

--- src/main/java/tagman/view/TimeView.java
+++ src/main/java/tagman/view/TimeView.java
@@ -1,11 +1,18 @@
 package tagman.view;
 
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class TimeView extends JPanel {
+public class TimeView extends JPanel implements Observer {
 
 	public TimeView() {
+		
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
 
 	}
 
