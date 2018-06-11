8652c18915fe8ccbc452c02dbfba9fd4c94cd61e
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-06-11 10:56:53 +0200

    Add basic painting of GameObjects in PlayView

--- src/main/java/tagman/view/TagManPainterPlain.java
+++ src/main/java/tagman/view/TagManPainterPlain.java
@@ -1,18 +1,42 @@
 package tagman.view;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import tagman.model.TagMan;
 
 public class TagManPainterPlain {
-	TagMan tagMan;
-
-	public TagManPainterPlain (TagMan tagMan) {
-		this.tagMan = tagMan;
+	
+	protected static void paint(Graphics g, TagMan tagMan, boolean won) {
+		Rectangle hitbox = tagMan.getHitbox();
+		
+		Color outerColor = won ? Color.GREEN.darker().darker() : Color.RED;
+		Color middleColor = won ? Color.GREEN.darker() : Color.ORANGE;
+		Color innerColor = won ? Color.GREEN : Color.YELLOW;
+		
+		drawPart(g, hitbox, outerColor);
+		drawPart(g, hitbox, middleColor, 8);
+		drawPart(g, hitbox, innerColor, 24);
 	}
 	
-	protected void paint (Graphics g) {
-		
+	protected static void paint(Graphics g, TagMan tagMan) {
+		paint(g, tagMan, false);
+	}
+	
+	private static void drawPart(Graphics g, Rectangle hitbox, Color color, int smaller) {
+		int offset = smaller / 2;
+		int x = hitbox.x + offset;
+		int y = hitbox.y + offset;
+		int width = hitbox.width - smaller;
+		int height = hitbox.height - smaller;
+
+		g.setColor(color);
+		g.fillOval(x, y, width, height);
+	}
+	
+	private static void drawPart(Graphics g, Rectangle hitbox, Color color) {
+		drawPart(g, hitbox, color, 0);
 	}
 
 }
