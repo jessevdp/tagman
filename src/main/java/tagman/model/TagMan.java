bf342067c17bbdce1f4d25611a11363855399b85
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-06-11 13:42:38 +0200

    Create move method in TagMan

--- src/main/java/tagman/model/TagMan.java
+++ src/main/java/tagman/model/TagMan.java
@@ -2,17 +2,43 @@ package tagman.model;
 
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Rectangle;
 
 public class TagMan extends GameObject {
 	
-	private static final int DEFAULT_SIZE = 50;
+	private static final int SIZE = 50;
+	private static final int MAX_SPEED = 3;
 	
 	public TagMan(Point position) {
-		super(position, new Dimension(DEFAULT_SIZE, DEFAULT_SIZE));
+		super(position, new Dimension(SIZE, SIZE));
 	}
 	
 	public TagMan(int x, int y) {
 		this(new Point(x, y));
+	}
+	
+	public void move(Direction direction) {
+		int speed = getSpeed(direction);
+		Rectangle hitbox = getHitbox();
+		int x = hitbox.x + (speed * direction.getHorizontalDirection());
+		int y = hitbox.y + (speed * direction.getVerticalDirection());
+		setPosition(x, y);
+	}
+	
+	/**
+	 * Applies potential modifications to the MAX_SPEED
+	 * to come to the actual speed. (If TagMan moves diagonal
+	 * his speed becomes 2/3 of the MAX_SPEED.)
+	 * 
+	 * @param direction
+	 * @return speed
+	 */
+	private int getSpeed(Direction direction) {
+		int speed = MAX_SPEED;
+		if (direction == Direction.NORTH_EAST || direction == Direction.SOUTH_EAST) {
+			speed = (2 / 3) * speed;
+		}
+		return speed;
 	}
 
 }
