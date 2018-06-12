c1bcbd39949cd07a73c6c4ae5ef079de4a9f5f09
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-06-12 11:00:47 +0200

    Make MainFrame able to listen to key events

--- src/main/java/tagman/view/MainFrame.java
+++ src/main/java/tagman/view/MainFrame.java
@@ -1,17 +1,24 @@
 package tagman.view;
 
 import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
+import tagman.controller.InputController;
 import tagman.controller.MainController;
 
 @SuppressWarnings("serial")
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements KeyListener {
 	
 	ContentPane contentPane;
+	MainController mainController;
+	InputController inputController;
 
 	public MainFrame (MainController mainController) throws HeadlessException {
+		this.mainController = mainController;
+		this.inputController = mainController.getInputController();
 		this.contentPane = new ContentPane(mainController);
 		
 		setContentPane(contentPane);
@@ -19,9 +26,28 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		
+		setFocusable(true);
+	    requestFocus();
+	    addKeyListener(this);
+		
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		inputController.handleKeyPress(e);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		inputController.handleKeyRelease(e);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		inputController.handleKeyType(e);
 	}
 
 }
